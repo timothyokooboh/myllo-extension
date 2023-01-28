9c519e452280046b972495fa7e4e59ce6d5c9169
timothy okooboh <okoobohtimothy@gmail.com>
2023-01-28 11:43:33 +0100

    improve code readability

--- chrome/content.js
+++ chrome/content.js
@@ -1,12 +1,8 @@
 window.addEventListener("message", (event) => {
-    if (event.data.type === "myllo_auth_user") {
-       
-
-        if(chrome.runtime?.id){
-            console.log(event.data)
-            chrome.storage.local.set({
-                'myllo_auth_user': event.data.user
-            })
-        }
-    }
-})
+  if (event.data.type === "myllo_auth_user" && chrome.runtime?.id) {
+    console.log(event.data);
+    chrome.storage.local.set({
+      myllo_auth_user: event.data.user,
+    });
+  }
+});
