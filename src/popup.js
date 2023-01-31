ae578372825098667a45c94908ee0db6943c9071
timothy okooboh <timothyokooboh@timothys-MacBook-Pro.local>
2023-01-31 20:19:51 +0100

    improve experience

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,16 @@
 import { functions } from "./firebase";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 const form = document.querySelector("#form");
 const titleField = document.querySelector("#title");
 const urlField = document.querySelector("#url");
 const categoryField = document.querySelector("#category");
 const switchBtn = document.querySelector("#switch-btn");
+const submitBtn = document.querySelector("#submit-btn");
+
 let isVisibilityPublic = true;
+let user = null;
 
 switchBtn.addEventListener("click", () => {
   isVisibilityPublic = !isVisibilityPublic;
@@ -17,37 +22,90 @@ switchBtn.addEventListener("click", () => {
   }
 });
 
-let user = null;
+const notify = (param) => {
+  const { message, type } = param;
+
+  const toastPayload = {
+    text: message,
+    duration: 6000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    // onClick: function(){} // Callback after click
+    style: {
+      width: "400",
+    },
+  };
+
+  if (type === "error") {
+    toastPayload.style = {
+      ...toastPayload.style,
+      background: "#000",
+      color: "#FFF",
+    };
+  }
+
+  Toastify(toastPayload).showToast();
+};
 
 const handleSubmit = async (event) => {
   event.preventDefault();
 
   if (user) {
-    const addResource = functions.httpsCallable("addResource");
-    const addToReadingList = functions.httpsCallable("addToReadingList");
+    try {
+      submitBtn.textContent = "Loading...";
+      submitBtn.classList.add("loading");
 
-    return;
+      const addResource = functions.httpsCallable("addResource");
+      const addToReadingList = functions.httpsCallable("addToReadingList");
 
-    const res = await addResource({
-      title: titleField.value,
-      url: urlField.value,
-      category: categoryField.value,
-      visibility: isVisibilityPublic ? "public" : "private",
-      description: "",
-      tags: [],
-      user,
-    });
+      console.log({
+        title: titleField.value,
+        url: urlField.value,
+        category: categoryField.value,
+        visibility: isVisibilityPublic ? "public" : "private",
+        description: "",
+        tags: [],
+        user,
+      });
 
-    console.log(res.data);
+      const res = await addResource({
+        title: titleField.value,
+        url: urlField.value,
+        category: categoryField.value,
+        visibility: isVisibilityPublic ? "public" : "private",
+        description: "",
+        tags: [],
+        user,
+      });
 
-    const result = addToReadingList({
-      resource_id: res.data.doc_ref_id,
-      author_id: user.uid,
-      uid: user.uid,
-    });
-    console.log(result.data);
+      console.log(res.data);
+
+      const result = await addToReadingList({
+        resource_id: res.data.doc_ref_id,
+        author_id: user.uid,
+        uid: user.uid,
+      });
+
+      console.log(result.data);
+      notify({ message: "You added to your reading list 👍", type: "success" });
+
+      setTimeout(() => {
+        window.close();
+      }, 5000);
+      window.close();
+    } catch (err) {
+      notify({
+        message: "Something went wrong. Please try again",
+        type: "error",
+      });
+    } finally {
+      submitBtn.textContent = "Save Resource";
+      submitBtn.classList.remove("loading");
+    }
   } else {
-    alert("you must be signed in");
+    // alert("you must be signed in");
   }
 };
 
