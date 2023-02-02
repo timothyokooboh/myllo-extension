fb86935eb4bd86f80ab7562f7f2a70a2d8b985fd
timothy okooboh <timothyokooboh@timothys-MacBook-Pro.local>
2023-02-02 17:31:43 +0100

    notify myllo web app that a resource has been added

--- chrome/content.js
+++ chrome/content.js
@@ -1,6 +1,5 @@
 window.addEventListener("message", (event) => {
   if (event.data.type === "myllo_auth_user" && chrome.runtime?.id) {
-    console.log(event.data);
     chrome.storage.local.set({
       myllo_auth_user: event.data.user,
     });
